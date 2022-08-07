3d46184783dc9f263007f986f72b152ab6259355
monvanCe <omerfkoca@gmail.com>
2022-08-07 04:45:05 +0300

    navigationsetup2

---
+++ src/screens/Onboarding101.js
@@ -0,0 +1,46 @@
+// libraries
+import React from 'react'
+import { View, StatusBar, Text, Image} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+// components
+import styles from '../components/Text'
+import Button from '../components/Button'
+
+
+
+export default () => {
+    return (
+        <SafeAreaView>
+
+            <StatusBar barStyle="dark-content" backgroundColor={'transparent'}/>
+
+            <View>
+                <Image
+                    style={{
+                        marginTop: '4%',
+                        width: '100%',
+                        height: '63.6%',
+                        resizeMode: 'stretch',
+                    }}
+                    source={require('./assets/Onboardin101-Carousel2.png')}
+                />
+
+                <Text style={[styles.mtext, {textAlign: 'center', marginTop: '12%'}]}>
+                    {'Welcome to CaStore !'}
+                </Text>
+
+                <Text style={[styles.stext, {textAlign: 'center', marginTop: '3%'}]}>
+                    {'With long experience in the audio industry,'}
+                </Text>
+
+                <Text style={[styles.stext, {textAlign: 'center', marginTop: '0.5%', marginBottom: '8%'}]}>
+                    {'we create the best quality products'}
+                </Text>
+
+                <Button text='Get Started' icon={require('./assets/LongArrow.png')} />
+            </View>
+                
+        </SafeAreaView>
+    )
+}
