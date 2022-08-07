34a792b5b201ee17edd714e402bc11b007813607
monvanCe <omerfkoca@gmail.com>
2022-08-07 05:43:59 +0300

    Signing1

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,12 +3,12 @@ import { View,Image,Text } from 'react-native'
 
 export default () => {
     return (
-        <View style={{flexDirection: 'row',}} >
+        <View style={{flexDirection: 'row', alignItems: 'center'}} >
             <Image
-                style={{}}
+                style={{width: 24, height: 24}}
                 source={require('../assets/Location.png')}
             />
-            <Text style={{fontSize:12, fontWeight: '700', alignItems: 'center'}}>Hanoi, Vietnam</Text>
+            <Text style={{fontSize:12, fontWeight: '700', color: 'black', justifyContent: 'center', marginLeft: '1.5%' }}>Hanoi, Vietnam</Text>
         </View>
     )
 }
--- src/screens/Signing102.js
+++ src/screens/Signing102.js
@@ -1,16 +1,31 @@
 import React from 'react'
-import { StatusBar } from 'react-native'
+import { StatusBar, View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+//components
 import Location from '../components/Location'
+import styles from '../components/Text'
 
 export const Signing = () => {
     return (
         <SafeAreaView>
 
-            <StatusBar barStyle="dark-content" backgroundColor={'transparent'}/>       
-            <Location/>
-            
+            <StatusBar barStyle="dark-content" backgroundColor={'transparent'}/> 
+
+            <View>
+
+                <Location/>
+
+                <Text style={[styles.mtext, {}]}>
+                    {"Let's Sign You In"}
+                </Text>
+
+                <Text style={[styles.stext,{}]}>
+                    {"Welcome back, you'have been missed!"}
+                </Text>
+
+            </View>
+
         </SafeAreaView>
     )
 }   
